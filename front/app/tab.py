e3eb1d071a927d5f0488c528df14280864d8a349
BelisariusArchMagus390 <bartsimpsom300@gmail.com>
2023-07-23 16:43:06 -0300

    Adicionado número de linhas

--- front/app/tab.py
+++ front/app/tab.py
@@ -1,8 +1,62 @@
+import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 from pathlib import Path
 import os
+
+
+class TextLineNumbers(tk.Canvas):
+    def __init__(self, *args, **kwargs):
+        tk.Canvas.__init__(self, *args, **kwargs)
+        self.textwidget = None
+
+    def attach(self, text_widget):
+        self.textwidget = text_widget
+
+    def redraw(self, *args):
+        """redraw line numbers"""
+        self.delete("all")
+
+        i = self.textwidget.index("@0,0")
+        while True:
+            dline = self.textwidget.dlineinfo(i)
+            if dline is None:
+                break
+            y = dline[1]
+            linenum = str(i).split(".")[0]
+            self.create_text(2, y, anchor="nw", text=linenum)
+            i = self.textwidget.index("%s+1line" % i)
+
+
+class CustomText(tk.Text):
+    def __init__(self, *args, **kwargs):
+        tk.Text.__init__(self, *args, **kwargs)
+
+        # create a proxy for the underlying widget
+        self._orig = self._w + "_orig"
+        self.tk.call("rename", self._w, self._orig)
+        self.tk.createcommand(self._w, self._proxy)
+
+    def _proxy(self, *args):
+        # let the actual widget perform the requested action
+        cmd = (self._orig,) + args
+        result = self.tk.call(cmd)
+
+        # generate an event if something was added or deleted,
+        # or the cursor position changed
+        if (
+            args[0] in ("insert", "replace", "delete")
+            or args[0:3] == ("mark", "set", "insert")
+            or args[0:2] == ("xview", "moveto")
+            or args[0:2] == ("xview", "scroll")
+            or args[0:2] == ("yview", "moveto")
+            or args[0:2] == ("yview", "scroll")
+        ):
+            self.event_generate("<<Change>>", when="tail")
+
+        # return what the actual widget returned
+        return result
 
 
 class Tab_editor:
@@ -25,7 +79,7 @@ class Tab_editor:
         self.hor_scroll = ttk.Scrollbar(self.frame, orient="horizontal")
 
         # Text Box
-        self.my_text = Text(
+        self.my_text = CustomText(
             self.frame,
             width=97,
             height=23,
@@ -45,7 +99,12 @@ class Tab_editor:
         self.ver_scroll.pack(side="right", fill="y")
         self.hor_scroll.pack(side="bottom", fill="x")
 
-        self.my_text.pack()
+        self.linenumbers = TextLineNumbers(self.frame, width=20)
+        # self.linenumbers.itemconfig(self.linenumbers, fill="#F91A1A")
+        self.linenumbers.attach(self.my_text)
+
+        self.linenumbers.pack(side="left", fill="y")
+        self.my_text.pack(side="right", fill="both", expand=True)
 
         self.my_notebook.add(self.frame, text="New File")
 
@@ -56,6 +115,12 @@ class Tab_editor:
         self.selected = False
 
         self.output_terminal = Frame()
+
+        self.my_text.bind("<<Change>>", self._on_change)
+        self.my_text.bind("<Configure>", self._on_change)
+
+    def _on_change(self, event):
+        self.linenumbers.redraw()
 
     def change_text(self, tittle):
         self.my_notebook.tab(self.frame, text=tittle)
