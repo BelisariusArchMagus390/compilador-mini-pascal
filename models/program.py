a89a67c7556fd4e97378fee799fccdb042e6fd5c
Sp2ceM0nk <42844749+BelisariusArchMagus390@users.noreply.github.com>
2023-12-02 16:00:07 -0300

    Edit some documents

--- models/program.py
+++ models/program.py
@@ -42,7 +42,7 @@ class Program:
                     print(err_msg)
                     should_exit = True
             case "2":
-                codigo = Program.read_file("teste19.txt")
+                codigo = Program.read_file("teste2.txt")
             case "0":
                 should_exit = True
             case _:
